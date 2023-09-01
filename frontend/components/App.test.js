94387afea44b4419159cea8a80f8c4044c6e36ef
sanjiro <defconx00@gmail.com>
2023-08-31 20:41:52 -0400

    testing more

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import AppClass from './AppClass'; // Import your component here
+import AppClass from './AppClass'; //
+import AppFunctional from './AppFunctional';
 
 test('sanity', () => {
   expect(true).toBe(true)
@@ -53,4 +54,29 @@ test('reset button clears messages', async () => {
     // Check that the response message is no longer in the document
     const responseElement = screen.queryByText(/You can't go/);
     expect(responseElement).not.toBeInTheDocument();
-  });
+});
+  
+test('App Class Renders', () => {
+    render(<AppFunctional />)
+  })
+  test('Nav links are present', () => {
+    render(<AppFunctional />)
+    const nav = screen.queryAllByRole('nav')
+    expect(nav).toBeTruthy()
+  })
+  test('Coordinates are present', () => {
+    render(<AppFunctional />)
+    const header = screen.queryByText(/Coordinates/i)
+    expect(header).toBeTruthy()
+  })
+  test('Buttons are present', () => {
+    render(<AppFunctional />)
+    const buttons = screen.queryAllByRole('button')
+    expect(buttons).toHaveLength(6)
+  })
+  test('Movement is present', () => {
+    render(<AppFunctional />)
+    const movement = screen.queryByText(/You moved/i)
+    expect(movement).toBeTruthy()
+  })
+
